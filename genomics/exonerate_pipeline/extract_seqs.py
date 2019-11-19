6870a4c5522fac4117817806f4094623da7563ba
Juan Felipe Ortiz <juanfortizq@gmail.com>
2019-11-19 11:02:12 +0900

    first prototype

--- genomics/exonerate_pipeline/extract_seqs.py
+++ genomics/exonerate_pipeline/extract_seqs.py
@@ -3,35 +3,42 @@ import gffutils
 #import pyfaidx
 from Bio import SeqIO
 from HTSeq import GFF_Reader
+import textwrap
+from ipdb import set_trace
 
 genome = sys.argv[1]
 gff = sys.argv[2]
-cds = sys.argv[3]
-prot = sys.argv[4]
+cds_file = sys.argv[3]
+prot_file = sys.argv[4]
 
 # Clean exonerate output
 gff_str=""
 gene_ct = 1
-for feat in GFF_Reader("meh.gff"):
+for feat in GFF_Reader(gff):
     if feat.type == "gene":
-        gene = "gene" + str(gene_ct)
+        query_name = feat.attr["sequence"].strip()
+        gene = f"{query_name}_{str(gene_ct)}"
         gene_ct += 1
         name = gene
         count = 1
     elif feat.type == "cds":
-        name = f"{gene}_{count}"
+        name = f"{gene}.{count}"
         count += 1
     chrom = feat.iv.chrom
     start = feat.iv.start + 1
     end = feat.iv.end
-    phase = feat.get_gff_line().split("\t")[7]
+    #phase = feat.get_gff_line().split("\t")[7]
+    phase = feat.frame
     endings = dict(gene="\n",cds=f";Parent={gene}\n")
     if feat.type in ["gene", "cds"]:
         ending = endings[feat.type]
         gff_str += f"{chrom}\texonerate\t{feat.type}\t{start}\t{end}\t.\t{feat.iv.strand}\t{phase}\tID={name}{ending}"
 
-db = gffutils.create_db(gff_str,dbfn=":memory:",from_string=True)
-#fasta = pyfaidx.Fasta(genome)
+parsed_gff_file = gff.replace(".gff", "_parsed.gff")
+with open(parsed_gff_file, "w") as gff_o:
+    gff_o.write(gff_str)
+
+db = gffutils.create_db(parsed_gff_file, dbfn=":memory:")
 fasta = genome
 cds_seq = ""
 cds_d = {}
@@ -41,3 +48,20 @@ for gene in db.features_of_type("gene", order_by="start"):
     cds_d[gene_name] = ""
     for cds in db.children(gene_name, featuretype="cds"):
         cds_d[gene_name] += cds.sequence(genome)
+
+# Save CDS sequences
+with open(cds_file, "w") as cds_f:
+    for gene, seq in cds_d.items():
+        cds_f.write(f">{gene}\n")
+        wrapped_seq = textwrap.fill(seq, 70) + "\n"
+        cds_f.write(wrapped_seq)
+
+# Translate and save protein sequences
+prots = []
+for seq in SeqIO.parse(cds_file, "fasta"):
+    prot_s = seq.translate()
+    prot_s.id = seq.id
+    prot_s.description = ""
+    prots.append(prot_s)
+
+SeqIO.write(prots, prot_file, "fasta")
--- genomics/exonerate_pipeline/run_exonerate.sh
+++ genomics/exonerate_pipeline/run_exonerate.sh
@@ -2,10 +2,18 @@
 
 # This script runs the analysis
 # Usage:
-#bash run.sh query_sequence.fa target_genome.fa
+#bash run_exonerate.sh query_sequence.fa target_genome.fa cds_file protein_file
 
 query=$1
 genome=$2
-
+cds_file=$3
+prot_file=$4
+exonerate_out=exonerate.txt
 # Run exonerate
-exonerate --model protein2genome -q $query -t $genome --showalignment no --showvulgar no --showtargetgff yes > exonerate_out
+exonerate --model protein2genome -q $query -t $genome --showalignment no --showvulgar no --showtargetgff yes > $exonerate_out
+exonerate_gff=`basename $exonerate_out .txt`.gff
+#cat $exonerate_out | grep -v Command | grep -v Hostname  | grep -P -v "\tsimilarity\t" | grep -v "#" | grep -v splice > $exonerate_gff
+cat $exonerate_out | grep -v Command | grep -v Hostname  | grep -v "\-\- completed exonerate analysis" > $exonerate_gff
+
+# Parse exonerate output and generate sequence files
+python extract_seqs.py $genome $exonerate_gff $cds_file $prot_file
