849d5ff816b3ec6b6bcc670176681153c5f81967
Juan Felipe Ortiz <juanfortizq@gmail.com>
2019-12-20 11:39:02 +0900

    add custom options variable in run_exonerate

--- genomics/exonerate_pipeline/run_exonerate.sh
+++ genomics/exonerate_pipeline/run_exonerate.sh
@@ -16,9 +16,15 @@ gff_out=$6
 
 
 # Run exonerate
+
+# Exonerate extra options
+# Modify the following variable with more options from exonerate. NOTE: Only change
+# what is inside the double quotes
+exonerate_options=""
+
 # Exonerate comes with a variety of models to be run. Here, the model is self
 # explanatory. More information can be found at https://www.ebi.ac.uk/about/vertebrate-genomics/software/exonerate-user-guide
-exonerate --model protein2genome -q $query -t $genome --showalignment no --showvulgar no --showtargetgff yes > $exonerate_out
+exonerate --model protein2genome $exonerate_options -q $query -t $genome --showalignment no --showvulgar no --showtargetgff yes > $exonerate_out
 # Replace the extension of the raw exonerate output, and append .gff. This
 # will be the name of the final GFF
 exonerate_gff=$(echo "$exonerate_out" | cut -f 1 -d '.').gff
